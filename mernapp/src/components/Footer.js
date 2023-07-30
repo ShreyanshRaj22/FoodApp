10a650ebfd491a8ac500b0d39dd0ed3f4e8669f3
Shreyansh Raj <211230051@nitdelhi.ac.in>
2023-07-30 18:10:36 +0530

    FrontEnd Change

--- mernapp/src/components/Footer.js
+++ mernapp/src/components/Footer.js
@@ -4,13 +4,8 @@ import { Link } from 'react-router-dom'
 export default function Footer() {
   return (
     <div>
-    <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <div className="col-md-4 d-flex align-items-center">
-      <Link to="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-        
-      </Link>
-      <span className="mb-3 mb-md-0 text-muted">© 2023 GoFood, Inc</span>
-    </div>
+    <footer className="text-center py-3 my-4 border-top" >
+      <span className="mb-3 mb-md-0 text-muted text-center">© 2023 GoFood, Inc</span>
   </footer>
     </div>
   )
