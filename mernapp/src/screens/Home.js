fd53d971c78078f95ff7442f9d89ef7d8430c243
Shreyansh Raj <211230051@nitdelhi.ac.in>
2023-08-12 16:01:37 +0530

    Few Changes

--- mernapp/src/screens/Home.js
+++ mernapp/src/screens/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
   const [foodItem, setFoodItem] = useState([]);
 
   const loadData = async () => {
-    let response = await fetch("https://server4-3jas.onrender.com/", {
+    let response = await fetch("https://server4-3jas.onrender.com", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
