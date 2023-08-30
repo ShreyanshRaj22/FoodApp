7b734a41c52e0ba6a23d899f065e5bd98ac99e74
Shreyansh Raj <211230051@nitdelhi.ac.in>
2023-08-30 23:24:22 +0530

    Testing

--- mernapp/src/screens/Cart.js
+++ mernapp/src/screens/Cart.js
@@ -18,7 +18,7 @@ export default function Cart() {
   const handleCheckOut = async()=>{
     let userEmail = localStorage.getItem("userEmail");
     console.log(data,localStorage.getItem("userEmail"),new Date())
-    let response = await fetch("https://food-app-backend-6jgw.vercel.app/",{
+    let response = await fetch("https://food-app-backend-rho.vercel.app/",{
       method:'POST',
       headers:{
         'Content-Type':'application/json'
